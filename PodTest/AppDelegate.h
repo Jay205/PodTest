b16eb2583be62fce029cbf8f3b2a3674a84005ae
abc <abc@abcdeimac.local>
2016-03-14 17:08:54 +0800

    Initial Commit

---
+++ PodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTest
+//
+//  Created by abc on 16/3/14.
+//  Copyright © 2016年 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
